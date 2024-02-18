592e6645dea7b5d7f489057c48ef331f46bb38f9
Volodymyr Buzanov <vbuzanov@ukr.net>
2024-02-18 22:42:06 +0200

    home task

--- back/src/route/index.js
+++ back/src/route/index.js
@@ -7,12 +7,21 @@ const router = express.Router()
 // Підключіть інші файли роутів, якщо є
 
 // Об'єднайте файли роутів за потреби
+
 // router.use('/', test)
 // Використовуйте інші файли роутів, якщо є
 
 router.get('/', (req, res) => {
   res.status(200).json('Hello World')
 })
+
+const auth = require('./auth')
+const balance = require('./balance')
+// const user = require('./user')
+
+router.use('/', auth)
+router.use('/', balance)
+// router.use('/', user)
 
 // Експортуємо глобальний роутер
 module.exports = router
